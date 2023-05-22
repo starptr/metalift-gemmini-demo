e565e6af138630d7e09ca4a05d4f3b34d9c0f973
metalift Demo <metalift@a3.Millennium.Berkeley.EDU>
2023-05-21 18:58:08 -0700

    Init commit

---
+++ metalift-demo-2-synth.sh
@@ -0,0 +1,19 @@
+
+# Use `bash -i`
+cd /scratch/metalift2
+cd tests
+use-llvm@11
+#./compile-add-blocks conv1d.cc
+./compile-add-blocks conv1d_2.cc
+cd ../
+#poetry run python3 tests/conv1d.py
+poetry run python3 tests/conv1d_2.py
+
+#cd /scratch/metaliftDeps/chipyard
+#source env.sh
+#cd generators/gemmini
+#cd software/gemmini-rocc-tests
+#./build.sh
+#cd ../../
+#./scripts/run-vcs.sh conv1d_synth
+#./scripts/run-vcs.sh conv1d_2_synth
